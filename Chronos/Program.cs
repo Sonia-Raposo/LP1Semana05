980023978a66d4fd69a7f244c7f488f35b1e1ec4
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-03-21 23:21:23 +0000

    Setting crono1

---
+++ Chronos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+
+            Stopwatch crono2 = new Stopwatch();
+
+
+            //Crono 1 Start
+            crono1.Start();
+
+            //Crono 1 Sleep 0.6s
+            Thread.Sleep(600);
+
+
+        }
+    }
+}
