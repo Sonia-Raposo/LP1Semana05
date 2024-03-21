50def7c2d1a81f349200984fa06c02a9fe20a86f
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-03-21 23:54:04 +0000

    Changed seed value to test different sequences

--- RandomTest/Program.cs
+++ RandomTest/Program.cs
@@ -6,8 +6,8 @@ namespace RandomTest
     {
         private static void Main(string[] args)
         {
-            Random r1 = new Random(22);
-            Random r2 = new Random(22);
+            Random r1 = new Random(20);
+            Random r2 = new Random(20);
             for (int i = 0; i < 4; i++)
                 Console.WriteLine(r1.Next());
             Console.WriteLine();
