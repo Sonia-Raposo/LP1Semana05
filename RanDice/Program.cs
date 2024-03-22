144ba37e0dd568a7dfadbc40152e5fbada1fd7c7
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-03-22 13:34:01 +0000

    Summary added

--- RanDice/Program.cs
+++ RanDice/Program.cs
@@ -2,6 +2,10 @@
 
 namespace RanDice
 {
+    /// <summary>
+    /// Accepts 2 int args, first being number of dice and second the seed
+    /// After inserted and enter, the result will be printed
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
@@ -25,12 +29,10 @@ namespace RanDice
                 int randomN = random.Next (1,7);
 
                 result += randomN;
-
             }
 
             //print dice result
             Console.WriteLine(result);
-
         }
     }
 }
