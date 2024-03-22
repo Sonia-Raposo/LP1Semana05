7fcc29b9a00425d6bc9dd8cb18344377f36e42f3
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-03-22 14:06:32 +0000

    Added TakeDmage

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -24,5 +24,25 @@ namespace MyGame
         {
             return name;
         }
+
+        /// <summary>
+        /// Damage is first inflicted on enemy shield, reducing its value
+        /// If the shield value becomes 0, damage now is inflicted on health
+        /// If damage inflicted lowers the enemy health bellow 0,
+        /// the health value becomes 0 to avoid a negative health value
+        /// </summary>
+        /// <param name="damage"></param>
+        public void TakeDamage (float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict =- shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0)
+                    health = 0;
+            }
+        }
     }
 }
