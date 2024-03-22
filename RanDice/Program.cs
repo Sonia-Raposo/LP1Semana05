0f3e9966945ef9a769637ff4a9cc3f0a2ae2700d
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-03-22 13:31:57 +0000

    For loop and print dice result

--- RanDice/Program.cs
+++ RanDice/Program.cs
@@ -6,7 +6,31 @@ namespace RanDice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //number of dice
+            int n = int.Parse(args[0]);
+
+            //number of seed
+            int s = int.Parse(args[1]);
+
+
+            //random class
+            Random random = new Random(s);
+
+            //dice result
+            int result = 0;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                int randomN = random.Next (1,7);
+
+                result += randomN;
+
+            }
+
+            //print dice result
+            Console.WriteLine(result);
+
         }
     }
 }
