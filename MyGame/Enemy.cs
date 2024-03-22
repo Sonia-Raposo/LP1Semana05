2c0993285b1505fb9fc54bdafd9daf8219df0ee4
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-03-22 13:55:53 +0000

    Added private variables and construtor enemy was added

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -1,12 +1,24 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace MyGame
 {
     public class Enemy
     {
+        //variables
+        private string name;
+        private float health;
+        private float shield;
         
+        public Enemy (string name)
+        {
+            this.name = name;
+            health = 100;
+            shield =0;
+        }
+      
     }
 }
